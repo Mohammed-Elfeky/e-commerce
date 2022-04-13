67276be91cf5578a15d717947bf99e90b0d525f3
Ro98h <52565633+Ro98h@users.noreply.github.com>
2022-04-14 00:13:46 +0200

    AdminInnerRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { AdminProductsComponent } from './components/admin-products/admin-products.component';
+import { AdminComponent } from './components/admin/admin.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
@@ -11,9 +13,15 @@ let routes:Routes=[
   {path:"category",component:CategoriesComponent},
   {path:"category/:id",component:ProductsComponent},
   {path:"product/:id",component:ProductDetailsComponent},
-  {path:"AdminProducts",component:AdminProductsComponent},
-  {path:"AdminAddProduct",component:ProductFormComponent},
-  {path:"AdminEditProduct/:id",component:ProductFormComponent}
+  {path:"Admin",component:AdminComponent,
+    children: [
+      {path: '', redirectTo: 'dashboard',pathMatch:'full'},
+      {path: 'dashboard', component: AdminDashboardComponent},
+      {path: 'products', component: AdminProductsComponent},
+      {path:"AdminAddProduct",component:ProductFormComponent},
+      {path:"AdminEditProduct/:id",component:ProductFormComponent}  
+    ]},
+  {path: '**', component: CategoriesComponent}
 ]
 
 @NgModule({
